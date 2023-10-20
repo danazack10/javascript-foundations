7f4538f82c72948760dcfa9a72193808635178c1
Dana Zack <danaezack@gmail.com>
2023-10-20 08:33:46 -0600

    Complete dragon test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,41 @@
+function createDragon(name, rider, temp){
+  var dragon = {
+    name: name,
+    rider: rider,
+    temperment: temp,
+    timesEaten: 0,
+    hungry: true
+  }
+  return dragon
+}
 
+function greetRider(dragon){
+  return `Hi, ${dragon.rider}!`
+}
+
+function eat(dragon){
+ dragon.timesEaten = dragon.timesEaten + 1
+ if (dragon.timesEaten < 3){
+  dragon.hungry = true
+ } else {
+  dragon.hungry = false
+ }
+ return dragon
+}
+
+function findFireBreathers(allDragons){
+  var allFireBreathers = []
+  for (var i = 0; i < allDragons.length; i++){
+    if (allDragons[i].temperment === "aggressive"){
+      allFireBreathers.push(allDragons[i])
+    }
+  }
+  return allFireBreathers;
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon, 
+  greetRider, 
+  eat, 
+  findFireBreathers
 }
