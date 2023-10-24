404436378d055fa260fca1aac3c372e42d78b4b4
Dana Zack <danaezack@gmail.com>
2023-10-23 19:08:21 -0600

    Complete birthday tests

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,30 @@
+function createBirthday(name, month, day) {
+    var birthday = {
+        name: name,
+        month: month,
+        day: day
+    }
+    return birthday
+}
 
+function celebrateBirthday(friendBirthday) {
+    return `Today is ${friendBirthday.month}/${friendBirthday.day}! Happy birthday, ${friendBirthday.name}!`
+}
 
-module.exports = {  };
+//birthdays is an array of objects
+//return the length of a new array
+function countBirthdays(birthdays, month) {
+    var count = [];
+    for (var i = 0; i < birthdays.length; i++){
+        if (birthdays[i].month === month) {
+            count.push(birthdays[i])
+        }
+    }
+    return count.length
+}
+
+module.exports = { 
+    createBirthday,
+    celebrateBirthday,
+    countBirthdays
+ };
